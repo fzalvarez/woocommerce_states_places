98bee0f3fff906f6b186b78506389ec20d40fee8
Fernando Zamudio <fzamudio.alvarez@gmail.com>
2020-05-19 14:01:03 -0500

    PE.php

---
+++ templates/states/PE.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ *  States of Perú
+ * - 24 States/Regiones
+ * 
+ * Sources:
+ * - https://es.wikipedia.org/wiki/Departamentos_del_Per%C3%BA
+ *
+ * @author  Fernando Zamudio <fzamudio.alvarez@gmail.com>
+ * @version 1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+global $states;
+
+$states['PE'] = array(
+  'AM' => 'Amazonas',
+  'AN' => 'Ancash',
+  'AP' => 'Apurímac',
+  'AR' => 'Arequipa',
+  'AY' => 'Ayacucho',
+  'CA' => 'Cajamarca',
+  'CU' => 'Cusco',
+  'HU' => 'Huancavelica',
+  'HC' => 'Huánuco',
+  'IC' => 'Ica',
+  'JU' => 'Junín',
+  'LL' => 'La Libertad',
+  'LA' => 'Lambayeque',
+  'LI' => 'Lima',
+  'LO' => 'Loreto',
+  'MD' => 'Madre de Dios',
+  'MO' => 'Moquegua',
+  'PA' => 'Pasco',
+  'PI' => 'Piura',
+  'PU' => 'Puno',
+  'SM' => 'San Martín',
+  'TA' => 'Tacna',
+  'TU' => 'Tumbes',
+  'UC' => 'Ucayali',
+);
